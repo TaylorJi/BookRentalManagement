eb9f25861de1178c17d0e0dba1e3286c94238c63
TaylorJi <sji24@my.bcit.ca>
2024-03-22 22:55:25 -0700

    added an endpoint for search by title for book

--- src/models/review.ts
+++
@@ -1,7 +0,0 @@
-import { ObjectId } from "mongoose";
-
-export default class Review {
-    constructor(
-        public name: string,
-        public summary: string,
-        public room_type: string,){}}
--- src/server.ts
+++ src/server.ts
@@ -20,36 +20,6 @@ mongoose.connect(dbUri)
 
 
 
-// interface IListing extends Document {
-//     maximum_nights: string;
-//     // add other properties as needed
-//   }
-  
-// const listingSchema: Schema = new mongoose.Schema({
-//     maximum_nights: { type: String }
-//   });
-  
-// const Listing = mongoose.model<IListing>('Listing', listingSchema, 'listingsAndReviews');
-  
-// app.get('/api/test', async (req: Request, res: Response) => {
-//     try {
-//       const listings = await Listing.find({ maximum_nights: "30" });
-//       console.log('Listings fetched successfully');
-//       console.log(listings.length);
-  
-//       res.json(listings);
-//     } catch (error) {
-//         if (error instanceof Error) {
-//             console.error('Error while fetching listings:', error);
-//             res.status(500).send('Error while fetching listings' + error);
-//         } else {
-//             console.error('Error while fetching listings:', error);
-//             res.status(500).send('Error while fetching listings');
-//         }
-      
-//     }
-//   });
-
   app.get('/api/genres', async (req: Request, res: Response) => {
     console.log('Fetching genres from the database');
     try {
@@ -67,6 +37,50 @@ mongoose.connect(dbUri)
     }
   });
 
+  app.get('/api/books', async (req: Request, res: Response) => {
+    console.log('Fetching books from the database');
+    try {
+      const books = await Book.find({});
+      console.log(`Found ${books.length} books`);
+      res.status(200).json(books);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error('Error while fetching listings:', error);
+            res.status(500).send('Error while fetching listings' + error);
+        } else {
+            console.error('Error while fetching listings:', error);
+            res.status(500).send('Error while fetching listings');
+        }
+    }
+  })
+
+
+  app.get('/api/books/search', async (req: Request, res: Response) => {
+    const {title} = req.query; // get the title from the query
+    console.log('Fetching books from the database');
+    if (title === undefined || title === '' || typeof title !== 'string') {
+        res.status(400).send('Invalid title');
+        return;
+        }
+        try {
+            const books = await Book.find({title: {$regex: title, $options: 'i'}});
+            console.log(`Found ${books.length} books`);
+            if (books.length === 0) {
+                res.status(404).send('Book not found');
+                return;
+            }
+            res.status(200).json(books);
+        } catch (error) {
+            if (error instanceof Error) {
+                console.error('Error while fetching listings:', error);
+                res.status(500).send('Error while fetching listings' + error);
+            } else {
+                console.error('Error while fetching listings:', error);
+                res.status(500).send('Error while fetching listings');
+            }
+        }
+  });
+
   app.post('/api/addBook', async (req: Request, res: Response) => {
     try {
         const {title, genre, rental_duration} = req.body;
@@ -79,7 +93,10 @@ mongoose.connect(dbUri)
       const savedBook = await book.save();
   
       console.log('Book added successfully');
-      res.json(genre);
+      res.status(201).json({
+        message: `${savedBook.title} Book added successfully`,
+        book: savedBook
+      });
     } catch (error) {
       console.error('Error while adding genre:', error);
       res.status(500).send('Error while adding genre' + error);
